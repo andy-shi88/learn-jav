548c596dd3d6ec9b2b35aa4517110c91895ca667
andy-shi88 <shi88.andy@gmail.com>
2017-10-13 11:56:31 +0700

    some code explaining

--- models/Main.java
+++ models/Main.java
@@ -2,11 +2,16 @@ import java.util.*;
 
 class Main {
 	public static void main(String[] args) {
+		/*Create response object for assignment*/
 		BaseResponse<Assignment> assignment_response = new BaseResponse<>();
+		/*Create response object for curricullum*/
 		BaseResponse<Curriculum> curricullum_response = new BaseResponse<>();
+		/*Create assignment objects*/
 		Assignment assignment_a = new Assignment("WPM", "https://10fastfingers.com");
 		Assignment assignment_b = new Assignment("Hackerrank", "https://hackerrank.com");
+		/*Create curriculum object*/		
 		Curriculum curriculum_a = new Curriculum("React Native", false);
+		/*Create the array and fill them*/
 		ArrayList<Assignment> assignments = new ArrayList<Assignment>();
 		ArrayList<Curriculum> curriculums = new ArrayList<Curriculum>();
 
@@ -15,6 +20,7 @@ class Main {
 		curriculums.add(curriculum_a);
 		assignment_response.setData(assignments);
 		curricullum_response.setData(curriculums);
+		/*Testing goes here*/
 		System.out.println("Assignment");
 		System.out.println(assignment_response.getData().get(0).getName());
 		System.out.println("Curriculum");
